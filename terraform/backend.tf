9548b7c0daad80d8a4cda59ee05b7aa4008bf2d3
OliuOliuHiu <hieuhd25022004@gmail.com>
2026-06-17 06:59:57 +0700

    chore: change config for deploy ci

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,10 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "os-k4-oliuhiu"
-    key = "lab-02-terra/dev/terraform.tfstate"
-    region = "ap-southeast-2"
+    bucket       = "os-k4-oliuhiu"
+    key          = "lab-02-tera/dev/terraform.tfstate"
+    region       = "ap-southeast-2"
     use_lockfile = true
-    encrypt = true
-    profile = "terraform"
+    encrypt      = true
   }
 }
